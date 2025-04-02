e3c3dc68146dd5ab86c669c174c5b4a941f26008
Sheritz Sogbesan <ssogbesan@gmail.com>
2025-04-02 15:43:53 -0400

    add nav partial and correct tmpl references

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,8 +12,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
 	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/pages/home.tmpl",
+		"../../ui/html/base.tmpl",
+		"../../ui/html/partials/nav.tmpl",
+		"../../ui/html/pages/home.tmpl",
 	}
 
 	ts, err := template.ParseFiles(files...)
