241cb6e3412845294a442eeda634005e20a138f0
Sheritz Sogbesan <ssogbesan@gmail.com>
2025-04-02 17:44:32 -0400

    add static files and styling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("../../ui/static/"))
+
+	// Register the file server as the handler for
+	// all URL paths starting with "/static/"
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
